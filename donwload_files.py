3a14f4c2bc45848c0cd08a7e14a34c91a2518ca0
Pablo9211 <josoriob1@gmail.com>
2023-02-01 22:33:56 -0500

    update dowload_files

--- donwload_files.py
+++ donwload_files.py
@@ -4,7 +4,7 @@ import requests
 def download_file(url):
     r = requests.get(url, stream=True)
     chunk_size = 2000
-    with open('/home/juan/Juan Pablo/Analisis_texto_IA/metadata.pdf', 'wb') as fd:
+    with open('metadata.pdf', 'wb') as fd:
         for chunk in r.iter_content(chunk_size):
             fd.write(chunk)
     return "succes"
