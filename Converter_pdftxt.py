6898f5d118cd1516378c9ab984a2a39cc65286ed
Pablo9211 <josoriob1@gmail.com>
2023-02-02 21:48:04 -0500

    update files

--- Converter_pdftxt.py
+++ Converter_pdftxt.py
@@ -1,10 +1,20 @@
 import PyPDF2
+import download_files2 as dw
 
-pdffileobj = open('resonance.pdf', 'rb')
+url1 = input("ingrese la url: ")
+filename = input("ingrese nombre del archivo (poner .pdf): ")
+
+dw.download(url1,filename)
+
+pdffileobj = open(filename, 'rb')
 pdfreader = PyPDF2.PdfReader(pdffileobj)
 x = len(pdfreader.pages)
-pageobj = pdfreader.pages[x - 1]
-text = pageobj.extract_text()
+i = 0
+while i<x:
+    pageobj = pdfreader.pages[i]
+    text = pageobj.extract_text()
+    i= i+1
+
 print(text)
 with open("2.txt","w", encoding="utf-8") as file1:
     file1.writelines(text)
--- download_files2.py
+++ download_files2.py
@@ -2,7 +2,9 @@ import urllib.request
 
 
 def download(url,namefile):
-
+    ''' esta funcion es para descargar archivos y recibe como variables
+        la url y el nombre de el archivo ( tener en cuenta enviar el nombre del archivo finalizado con .pdf)
+    '''
     urllib.request.urlretrieve(url, namefile)
 
 
